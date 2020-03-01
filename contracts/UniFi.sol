a0c34e6de4fc122794df3d259e74087e25e8fc7f
CJ42 <CJ42@users.noreply.github.com>
2020-03-01 09:24:55 +0000

    added Natspec comments

--- contracts/UniFi.sol
+++ contracts/UniFi.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.6.0;
 
 /// @title Unifi contract - EthLondon Hackathon
 /// @author Extropy.io team
-/// @notice You can use this contract to get paid for your wifi
+/// @notice You can use this contract to get paid for sharing your wifi
 /// @dev Are we going to implement ERC20 Token?
 contract UniFi {
     
@@ -45,13 +45,14 @@ contract UniFi {
         _;
     }
 
-
     /// Get user balance
     /// @return the user balance
     function getUserBalance() public view returns(uint) {
         return user_array[msg.sender].balance;
     }
     
+    /// @notice registering host (SSID: `_ssid`, MAC: `_mac_address`)
+    /// @dev 
     /// @param _longitude wifi longitude (geolocation)
     /// @param _latitude wifi latitude (geolocation)
     /// @param _ssid SSID of the wifi host
@@ -71,6 +72,8 @@ contract UniFi {
         host_array[msg.sender].balance = 0;
     }
     
+    /// @notice Congrats have been registered !
+    /// @dev Set the balance to 0 by default
     function registerUser() private isNotConnected(msg.sender){
         user_array[msg.sender] = User({
             is_connected: false,
@@ -79,10 +82,16 @@ contract UniFi {
         });
     }
     
+    /// @notice Congrats `_host`, you have been granted some reputation! New reputation = `_rep` + 1
+    /// @dev Set rep
+    /// @param _host The host to reward reputation
+    /// @param _rep the number of reputation to grant
     function repHost(address _host, uint8 _rep) internal {
         host_array[_host].rating[_rep] += 1;
     }
     
+    /// @notice Log-in successfully to the host
+    /// @dev Shouldn't we define to which host the user is logged in?
     function login() public isNotConnected(msg.sender) {
         if ( user_array[msg.sender].is_registered == false ) {
             registerUser();
@@ -91,6 +100,10 @@ contract UniFi {
         user_array[msg.sender].is_connected = true;
     }
     
+    /// @notice You have been logged out
+    /// @dev Same, shouldn't we specify the host that we log-out from?
+    /// @param user the user to log-out
+    /// @param charge the charge to apply
     function logout(address user, uint charge) public isConnected(user) {
         // subtract the charge
         user_array[user].balance -= charge;
@@ -102,10 +115,14 @@ contract UniFi {
     
 
     /// @notice you are about to top up your balance
+    /// @dev we shouldn't use the keyword `balance` for struct member 
+    ///      since it's a reserved keyword in Solidity (for clarity)
     function deposit() public payable {
         user_array[msg.sender].balance += msg.value;
     }
     
+    /// @notice 
+    /// @dev Use the pending withdraw pattern
     function withdrawHost() public {
         // subtract user usage from host balance
         uint amount = pendingWithdrawals[msg.sender];
@@ -115,6 +132,8 @@ contract UniFi {
         msg.sender.transfer(amount);
     }
     
+    /// @notice
+    /// @dev
     function withdrawUser() public {
         // subtract user usage from user balance
         uint amount = pendingWithdrawals[msg.sender];
@@ -125,8 +144,7 @@ contract UniFi {
     }
     
     /// Handle ethers received by the contract
-    receive() external payable {
-    }
+    receive() external payable {}
     
     /// Handle undefined function calls
     fallback() external {
